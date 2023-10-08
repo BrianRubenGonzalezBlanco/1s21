2ee05b1d33a6f4c095a5f30dc085ce150707b07b
BrianRubenGonzalezBlanco <144179421+BrianRubenGonzalezBlanco@users.noreply.github.com>
2023-10-08 14:00:09 -0600

    Add files via upload

---
+++ p2p1_1s21.java
@@ -0,0 +1,9 @@
+public class p2p1_1S21{
+	public static void main(String[] args) {
+		a=12;
+		if (a<10){
+			Sysem.ouy.println("si ingreso por que es verdadero");
+		}
+		System.ou.println("continuamos con el codigo");
+	}
+}
