1a2609a80382bdb740e85180dc1047919ad99415
BrianRubenGonzalezBlanco <144179421+BrianRubenGonzalezBlanco@users.noreply.github.com>
2023-10-10 14:32:59 -0600

    Add files via upload

---
+++ p1/p2_1s21.java
@@ -0,0 +1,10 @@
+class p2_1s21 {
+	public static void main(String[] args) {
+		System.out.println("datos personales");
+	    Nobre.println("Brian Ruben Gonzalez Blanco") 
+	    Edad.println("21")
+	    Correo.printnl("brianrubengb@gmail.com")
+	    Aficion.println("estudiante")
+	    Color favorito.println("azul") 
+	}
+}
